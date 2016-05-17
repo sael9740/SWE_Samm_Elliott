27ecba3a75afd3aa1abfb046f3175bb2fb5cfcb4
sael9740 <sael9740@colorado.edu>
2016-05-16 19:32:05 -0600

    transfer

--- Jacobi/jacobi.c
+++ Jacobi/jacobi.c
@@ -109,7 +109,7 @@ double max_diff(double A[DIMENSION][DIMENSION][DIMENSION],
         
     }
     printf("maximum difference: %f at (%d,%d,%d)\nf(i,j,k)=%f\n",max,imax,jmax,kmax,f(imax,jmax,kmax));
-    printf("value of A_max is %f and B_max is %f\n",A[imax][jmax][kmax],B[imax][jmax][kmax])
+    printf("value of A_max is %f and B_max is %f\n",A[imax][jmax][kmax],B[imax][jmax][kmax]);
     return max;
 }
 
