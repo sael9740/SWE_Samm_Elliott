ab7cb7e3c82f7b0b21952447d4afa3bac78376a3
sael9740 <sael9740@colorado.edu>
2016-05-17 20:19:50 -0600

    transfer

--- Jacobi/jacobi_acc.c
+++ Jacobi/jacobi_acc.c
@@ -44,7 +44,7 @@ int main(int argc, char** argv)
         
         do_jacobi(jacobi_A,jacobi_B);
         
-        *dummy = jacobi_A;
+        *dummy = (Domain_t *) jacobi_A;
         jacobi_A = jacobi_B;
         jacobi_B = *dummy;
         err = max_diff(jacobi_A,real_sol);
