b4f09b1e706c322fbab58790b8c5812f71cecad7
sael9740 <sael9740@colorado.edu>
2016-05-17 20:09:19 -0600

    transfer

--- Jacobi/jacobi_acc.c
+++ Jacobi/jacobi_acc.c
@@ -127,7 +127,7 @@ double max_diff(Domain_t A, Domain_t B)
     int i,j,k;
     double err, max_val = 0;
     
-    #pragma acc parallel loop private(j,k,err) reduction(max:max_val) independent collapse(3)
+    #pragma acc parallel loop private(j,k,err) reduction(max:max_val) independent collapse(3) present(A,B)
     for(i=0;i<DIMENSION;i++) {
         for(j=0;j<DIMENSION;j++) {
             for(k=0;k<DIMENSION;k++) {
@@ -146,7 +146,7 @@ void do_jacobi(Domain_t A, Domain_t B)
 {
     int i,j,k;
     
-    #pragma acc parallel loop private(j,k) independent collapse(3)
+    #pragma acc parallel loop private(j,k) independent collapse(3) present(A,B)
     for(i=1;i<DIMENSION-1;i++) {
         for(j=1;j<DIMENSION-1;j++) {
             for(k=1;k<DIMENSION-1;k++) {
